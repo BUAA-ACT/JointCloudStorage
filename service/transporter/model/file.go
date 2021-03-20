001ce776d6a36d74c777fdf09cc984ce67c86bf9
Zhang Junhua <zhangjh@act.buaa.edu.cn>
2021-03-20 08:56:09 +0800

    实现内存 File 存储

--- service/transporter/model/file.go
+++ service/transporter/model/file.go
@@ -18,6 +18,10 @@ type File struct {
 }
 
 type FileDatabase interface {
+	CreateFileInfo(file *File) (err error)
+	DeleteFileInfo(file *File) (err error)
+	UpdateFileInfo(file *File) (err error)
+	GetFileInfo(Id string) (file *File, err error)
 }
 
 type InMemoryFileDatabase struct {
@@ -40,4 +44,12 @@ func (fd *InMemoryFileDatabase) UpdateFileInfo(file *File) (err error) {
 	}
 	fd.db[file.Id] = *file
 	return nil
+}
+
+func (fd *InMemoryFileDatabase) GetFileInfo(Id string) (file *File, err error) {
+	f, ok := fd.db[Id]
+	if ok {
+		return &f, nil
+	}
+	return nil, errors.New("file info not exist")
 }
