e4f92110cd1371c70fcf30c3426642c9c227c112
wanggj <wanggj@mail.act.buaa.edu.cn>
2021-04-06 19:44:08 +0800

    mongo user interface

--- service/transporter/model/user.go
+++ service/transporter/model/user.go
@@ -2,6 +2,11 @@ package model
 
 import (
 	"act.buaa.edu.cn/jcspan/transporter/util"
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"time"
 )
 
@@ -43,6 +48,73 @@ type AccessCredential struct {
 type UserDatabase interface {
 	GetUserFromID(uid string) (*User, error)
 	UpdateUserInfo(user *User) error
+}
+
+type MongoUserDatabase struct {
+	databaseName   string
+	clientOptions  *options.ClientOptions
+	client         *mongo.Client
+	collectionName string
+}
+
+func NewMongoUserDatabase() (*MongoUserDatabase, error) {
+	var clientOptions *options.ClientOptions
+	if util.Config.Database.Username != "" {
+		clientOptions = options.Client().ApplyURI("mongodb://" + util.Config.Database.Username + ":" + util.Config.Database.Password + "@" +
+			util.Config.Database.Host + ":" + util.Config.Database.Port)
+	} else {
+		clientOptions = options.Client().ApplyURI("mongodb://" + util.Config.Database.Host + ":" + util.Config.Database.Port)
+	}
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+	return &MongoUserDatabase{
+		databaseName:   util.Config.Database.DatabaseName,
+		clientOptions:  clientOptions,
+		client:         client,
+		collectionName: "Task",
+	}, nil
+}
+
+func (m *MongoUserDatabase)UpdateUserInfo(user *User) error{
+	err := CheckClient(m.client, m.clientOptions)
+	if err != nil {
+		return err
+	}
+
+	filter:=bson.M{
+		"user_id":user.UserId,
+	}
+	update := bson.D{
+		{"$set", *user},
+	}
+	collection := m.client.Database(m.databaseName).Collection(m.collectionName)
+	_,err=collection.UpdateOne(context.Background(),filter,update)
+	if err!=nil{
+		return err
+	}else{
+		return nil
+	}
+}
+
+func (m *MongoUserDatabase)GetUserFromID(uid string) (*User, error){
+	err := CheckClient(m.client, m.clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	var result User
+
+	//get the collection and find by _id
+	collection := m.client.Database(m.databaseName).Collection(m.collectionName)
+	err = collection.FindOne(context.TODO(), bson.D{{"user_id", uid}}).Decode(&result)
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+	return &result, err
 }
 
 type InMemoryUserDatabase struct {
