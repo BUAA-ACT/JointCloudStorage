791366653dc0b8d9248c52f4670741822005e0bc
Huchao Xiong <xionghc@act.buaa.edu.cn>
2021-06-26 18:03:50 +0800

    Feature: Add AK, SK.

--- portal/src/components/userCenter/userInfo.vue
+++ portal/src/components/userCenter/userInfo.vue
@@ -3,16 +3,44 @@
     <el-button @click="getUserInfo" type="primary">获取用户信息</el-button><br />
     <!--    <JsonViewer :value="UserInfo"></JsonViewer>-->
 
-    <el-card class="box-card">
+    <el-card class="box-card user-info-container">
       <div slot="header" class="clearfix">
         <span>用户信息</span>
       </div>
-      用户名：{{ email }}
+      <el-form label-width="70px" label-position="left">
+        <el-form-item label="用户名">{{ email }}</el-form-item>
+        <el-form-item label="密码"><el-button @click="changePassword">修改密码</el-button></el-form-item>
+      </el-form>
     </el-card>
     <el-card class="box-card">
       <div slot="header" class="clearfix">
         <span>访问凭证</span>
       </div>
+      <el-card class="inner-box-card" v-for="AccessCredential in UserInfo.AccessCredentials" :key="AccessCredential.CloudID" shadow="hover">
+        <div slot="header" class="clearfix">
+          <span>{{ AccessCredential.CloudID }}</span>
+        </div>
+        <div class="text item">
+          访问用户：{{ AccessCredential.UserID }}<br />
+          访问密码：{{ AccessCredential.Password }}<br />
+        </div>
+      </el-card>
+    </el-card>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>访问凭证-NEW</span>
+      </div>
+      <el-card class="inner-box-card" shadow="hover">
+        <div slot="header" class="clearfix">
+          <span></span>
+        </div>
+        <div class="text item">
+          <el-form label-position="right">
+            <el-form-item label="Access Key"> <el-input :value="ak" class="no-border" size="large" /> </el-form-item>
+            <el-form-item label="Secret Key"> <el-input :value="sk" show-password class="no-border" size="large"/></el-form-item>
+          </el-form>
+        </div>
+      </el-card>
       <el-card class="inner-box-card" v-for="AccessCredential in UserInfo.AccessCredentials" :key="AccessCredential.CloudID" shadow="hover">
         <div slot="header" class="clearfix">
           <span>{{ AccessCredential.CloudID }}</span>
@@ -35,7 +63,10 @@ export default {
     return {
       UserInfo: {
         AccessCredentials: []
-      }
+      },
+      ak: "application_key",
+      sk: "SeCreTKey",
+      type: "text"
     };
   },
   methods: {
@@ -43,6 +74,9 @@ export default {
       common.getUserInfo(this.$store.getters.token).then(resp => {
         this.UserInfo = resp.UserInfo;
       });
+    },
+    async changePassword() {
+      // TODO
     }
   },
   computed: {
@@ -60,10 +94,18 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.user-info-container {
+  .el-form {
+    width: 50%;
+  }
+}
 .box-card {
   margin: 10px 0;
   .inner-box-card {
     width: 30%;
+    .no-border /deep/ .el-input__inner {
+      border: 0;
+    }
   }
 }
 </style>
