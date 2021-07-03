496f292d01b924c233894b30480a4781eca2bf04
xiaohan209 <xiaohan209@buaa.edu.cn>
2021-07-03 22:37:53 +0800

    token will be out of date

--- service/httpserver/dao/tokenDao.go
+++ service/httpserver/dao/tokenDao.go
@@ -68,12 +68,23 @@ func (d *Dao) CheckValid(token string) (string, bool) {
 		},
 	}}
 	var originToken model.AccessTokenCode
-	err := col.FindOneAndUpdate(context.TODO(), filter, update).Decode(&originToken)
-	if err != nil {
+	// find it and check its time
+	findErr := col.FindOne(context.TODO(), filter).Decode(&originToken)
+	if findErr != nil {
 		log.Println("无效token")
 		return "", false
 	}
-	return originToken.UserID, true
+	if timeNow.Sub(originToken.AccessTokenModifyTime).Hours() >= 4.0 || timeNow.Sub(originToken.AccessTokenCreateTime).Hours() >= 14*24.0 {
+		// out of date and need to delete
+		_, err := col.DeleteMany(context.TODO(), filter)
+		tools.PrintError(err)
+		return "", false
+	} else {
+		// in time and need to update
+		_, err := col.UpdateMany(context.TODO(), filter, update)
+		tools.PrintError(err)
+		return originToken.UserID, true
+	}
 }
 
 func (d *Dao) DeleteAccessToken(accessToken string) (*mongo.DeleteResult, bool) {
