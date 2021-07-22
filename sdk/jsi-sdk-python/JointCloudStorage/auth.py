89efb3bc71bd7fe8e1f24bf10f7b9d6bdb1a1229
xiaohan209 <xiaohan209@buaa.edu.cn>
2021-07-22 15:37:58 +0800

    write some bugs for zjh

--- sdk/jsi-sdk-python/JointCloudStorage/auth.py
+++ sdk/jsi-sdk-python/JointCloudStorage/auth.py
@@ -1,3 +1,24 @@
+import hashlib
+import base64
+import time
+
+HttpHeaderKeyAuthorization = "Authorization"
+HttpHeaderKeyTime = "Time"
+HttpHeaderMethod = "Method"
+HttpHeaderURL = "URL"
+
+
+def url_suffix(url):
+    return list(reversed(url.split("//")))[0].split('/', 1)[1]
+
+
+def sha3_encode(secret_key, sign):
+    h = hashlib.new('sha3_384')
+    h.update(bytes(secret_key, encoding='utf-8'))
+    h.update(bytes(sign, encoding='utf-8'))
+    return base64.urlsafe_b64encode(h.digest())
+
+
 class UserInfo(object):
     accessKey = ""
     accessSecret = ""
@@ -16,7 +37,7 @@ class AuthBase(object):
         self.user_info = user_info
         pass
 
-    def _sign_url(self, method, url) -> str:
+    def _sign_url(self, method, url, unix_time_now) -> str:
         return ""
 
     def _sign_request(self, req):
@@ -27,20 +48,34 @@ class Auth(AuthBase):
     """
     jcs 认证类
     """
+    keys = [HttpHeaderMethod, HttpHeaderURL, HttpHeaderKeyTime]
 
     def __init__(self, ak, sk):
         user_info = UserInfo(ak, sk)
         super().__init__(user_info)
 
-    def _sign_url(self, method, url) -> str:
+    def _sign_url(self, method, url, unix_time_now) -> str:
         """
         为指定的 method 和 url 生成签名字符串 access_token
         :param method: 如 “PUT”， “GET”
         :param url: 如 /path/to/file?param=xxx
+        :param unix_time_now: 如 1626938559 -> unix timestamp
         :return: 完整签名 token
         """
-        # todo 待编写
-        return ""
+
+        value_dict = {
+            HttpHeaderMethod: method,
+            HttpHeaderURL: url,
+            HttpHeaderKeyTime: time,
+        }
+        # generate the origin sign
+        sign = ""
+        for key in self.keys:
+            sign += key + ":" + str(value_dict[key]) + "\r\n"
+        # encode with sha-3-384
+        encode_sign = sha3_encode(self.user_info.accessSecret, sign)
+        authorization = "JCS(" + self.user_info.accessKey + ":" + str(encode_sign, encoding="utf-8") + ")"
+        return authorization
 
     def _sign_request(self, req):
         """
@@ -48,5 +83,12 @@ class Auth(AuthBase):
         :param req: 待签名 request 对象
         :return: 签名好的 request
         """
-        # todo 调用 _sign_url 方式获取签名 token，并设置到请求头内
-        pass
+        # get time
+        time_now = time.time()
+
+        # use _sign_url method to get authorization token
+        authorization = self._sign_url(method=req.method, url=url_suffix(req.url), unix_time_now=int(time_now))
+        # set authorization token in header
+        req.headers[HttpHeaderKeyAuthorization] = authorization
+        # set time in header
+        req.headers[HttpHeaderKeyTime] = str(int(time_now))
